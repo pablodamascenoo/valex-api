db7aebe8dbf7e210f7f868e0fb6b3578f9d8fcce
pablodamascenoo <pablofacex@gmail.com>
2022-07-12 10:48:44 -0300

    fix: fixed database ssl config

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,16 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-export const connection = new Pool({
+let configDatabase: pg.PoolConfig;
+
+configDatabase = {
     connectionString: process.env.DATABASE_URL,
-});
+};
+
+if (process.env.MODE === "PROD") {
+    configDatabase = {
+        ssl: { rejectUnauthorized: false },
+    };
+}
+
+export const connection = new Pool(configDatabase);
